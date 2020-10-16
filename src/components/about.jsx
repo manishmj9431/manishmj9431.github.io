b500a40e5a7c190b7a9c83db862cd9524f92f3c7
unknown <manishmj9431@gmail.com>
2020-10-16 21:06:48 +0530

    Initial commit

---
+++ src/components/about.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+import myImage from "../img/myImage.png";
+
+class About extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      skills: [
+        { id: "HTML5_skill", content: "HTML5", porcentage: "80%", value: "80" },
+        { id: "CSS3_skill", content: "CSS3", porcentage: "75%", value: "75" },
+        {
+          id: "JavaScript_skill",
+          content: "JavaScript",
+          porcentage: "90%",
+          value: "90",
+        },
+        { id: "PHP_skill", content: "PHP", porcentage: "70%", value: "70" },
+        {
+          id: "ReactJS_skill",
+          content: "ReactJS",
+          porcentage: "80%",
+          value: "80",
+        },
+        {
+          id: "Python_skill",
+          content: "Python",
+          porcentage: "75%",
+          value: "75",
+        },
+        {
+          id: "VanillaJS_skill",
+          content: "Machine Learning",
+          porcentage: "85%",
+          value: "85",
+        },
+        {
+          id: "Wordpress_skill",
+          content: "C++",
+          porcentage: "80%",
+          value: "80",
+        },
+      ],
+      about_me: [
+        {
+          id: "first-p-about",
+          content:
+            "Self-Motivated Electronics engineering undergraduate seeking opportunities in Software Development Role, Fast Learner and flexible in adapting with changing situations, Having an inclination towards Software development and Machine learning.",
+        },
+        {
+          id: "second-p-about",
+          content:
+            "Been the lead for an entrepreneurial project and have been shortlisted by a company for further development, the Project was an important phase as that taught me so much about business and financial modelling and finding innovative solutions to regular issues.",
+        },
+        {
+          id: "third-p-about",
+          content:
+            "Worked with a company working under factory automation and have been introduced to the challenges faced by the industry towards automation, also have presented some ideas to overcome the issues.",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <section id="about" className="about-mf sect-pt4 route">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="box-shadow-full">
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="row">
+                      <div
+                        className="col-sm-6 col-md-5"
+                        style={{ margin: "0 auto" }}
+                      >
+                        <div
+                          className="about-img"
+                          style={{ textAlign: "center" }}
+                        >
+                          <img
+                            className="img-fluid rounded b-shadow-a"
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="skill-mf">
+                      {/* <p className="title-s">Skill</p> */}
+                      {this.state.skills.map((skill) => {
+                        return (
+                          <React.Fragment key={skill.id}>
+                            <span>{skill.content}</span>{" "}
+                            <span className="pull-right">
+                              {skill.porcentage}
+                            </span>
+                            <div className="progress">
+                              <div
+                                className="progress-bar"
+                                role="progressbar"
+                                style={{ width: skill.porcentage }}
+                                aria-valuenow={skill.value}
+                                aria-valuemin="0"
+                                aria-valuemax="100"
+                              ></div>
+                            </div>
+                          </React.Fragment>
+                        );
+                      })}
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="about-me pt-4 pt-md-0">
+                      <div className="title-box-2">
+                        <h5 className="title-left">About Me</h5>
+                      </div>
+                      {this.state.about_me.map((content) => {
+                        return (
+                          <p className="lead" key={content.id}>
+                            {content.content}
+                          </p>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default About;
