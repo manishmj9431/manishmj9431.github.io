734c706088730135cad5403c5a3219f92630b05f
unknown <manishmj9431@gmail.com>
2020-11-24 17:08:12 +0530

    Updates

---
+++ precache-manifest.0a6a339e9e6734914bedc26a941d6f12.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84639f76dac3301cccafc24a7ed95db",
+    "url": "/aparnabarve121.github.io/index.html"
+  },
+  {
+    "revision": "722dba077318d085e68e",
+    "url": "/aparnabarve121.github.io/static/css/2.e2b7542f.chunk.css"
+  },
+  {
+    "revision": "7f5ca7e544379b19c3cf",
+    "url": "/aparnabarve121.github.io/static/css/main.aac68032.chunk.css"
+  },
+  {
+    "revision": "722dba077318d085e68e",
+    "url": "/aparnabarve121.github.io/static/js/2.403ec246.chunk.js"
+  },
+  {
+    "revision": "93c582b75377f64d74a3c583d830010f",
+    "url": "/aparnabarve121.github.io/static/js/2.403ec246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f5ca7e544379b19c3cf",
+    "url": "/aparnabarve121.github.io/static/js/main.e1a8e839.chunk.js"
+  },
+  {
+    "revision": "556643270871cb79b6a3",
+    "url": "/aparnabarve121.github.io/static/js/runtime-main.ce09d460.js"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/aparnabarve121.github.io/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/aparnabarve121.github.io/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/aparnabarve121.github.io/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/aparnabarve121.github.io/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/aparnabarve121.github.io/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/aparnabarve121.github.io/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "768998c65ce07675ede8536017d4f6ce",
+    "url": "/aparnabarve121.github.io/static/media/image1.768998c6.jpg"
+  },
+  {
+    "revision": "8de6195d8faafe31ac0a842eb0cbe1a9",
+    "url": "/aparnabarve121.github.io/static/media/image2.8de6195d.jpg"
+  },
+  {
+    "revision": "28470933ba170d7cd8d5495969722a5d",
+    "url": "/aparnabarve121.github.io/static/media/image3.28470933.jpg"
+  },
+  {
+    "revision": "0b37f43494ce5a35f3e1575612048750",
+    "url": "/aparnabarve121.github.io/static/media/image4.0b37f434.jpg"
+  },
+  {
+    "revision": "8d16c4aff0e7bd3db1b8bab9619aea9c",
+    "url": "/aparnabarve121.github.io/static/media/image5.8d16c4af.jpg"
+  },
+  {
+    "revision": "62b7cc7591147260c113823ded8f6901",
+    "url": "/aparnabarve121.github.io/static/media/image6.62b7cc75.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/aparnabarve121.github.io/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/aparnabarve121.github.io/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/aparnabarve121.github.io/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "c037dbbc0e6790f30e824a50010df5fb",
+    "url": "/aparnabarve121.github.io/static/media/ionicons.c037dbbc.svg"
+  },
+  {
+    "revision": "a82ab1cd3c28c0a07a4932d57176cb88",
+    "url": "/aparnabarve121.github.io/static/media/male.a82ab1cd.png"
+  },
+  {
+    "revision": "3ec3891b37f8b0765b554f10806f0b3f",
+    "url": "/aparnabarve121.github.io/static/media/male1.3ec3891b.png"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "/aparnabarve121.github.io/static/media/myImage.bd80794f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a31bad579bf89ec9b36c75d72da4f5d9.js"
+  "/aparnabarve121.github.io/precache-manifest.0a6a339e9e6734914bedc26a941d6f12.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aparnabarve121.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
